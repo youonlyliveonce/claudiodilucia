05ad0edd6e139525b12c6e11fd974bada68a8eaa
danielkremin <design@kremin.de>
2017-05-31 23:30:44 +0200

    Update

--- public/site/snippets/menu.php
+++ public/site/snippets/menu.php
@@ -1,26 +1,28 @@
 <nav class="Navigation" role="navigation">
-	<ul>
-		<?php foreach($site->page('home')->children()->visible() as $item): ?>
-			<?php if($item->navigation() == "main"): ?>
-				<?php if($item->intendedTemplate() == 'redirect'): ?>
-					<li>
-						<a href="<?= $item->redirect(); ?>" target="_blank"><span><?= $item->title()->html() ?><span></a>
-					</li>
-				<?php else : ?>
+	<div class="Navigation__inner">
+		<ul>
+			<?php foreach($site->page('home')->children()->visible() as $item): ?>
+				<?php if($item->navigation() == "main"): ?>
+					<?php if($item->intendedTemplate() == 'redirect'): ?>
+						<li>
+							<a href="<?= $item->redirect(); ?>" target="_blank"><span><?= $item->title()->html() ?><span></a>
+						</li>
+					<?php else : ?>
+						<li>
+							<a href="/<?= $site->language() ?>/?section=<?= $item->slug(); ?>"><span><?= $item->title()->html() ?><span></a>
+						</li>
+					<?php endif; ?>
+				<?php endif; ?>
+			<?php endforeach; ?>
+		</ul>
+		<ul class="Navigation__third">
+			<?php foreach($site->page('home')->children()->visible() as $item): ?>
+				<?php if($item->navigation() == "third"): ?>
 					<li>
 						<a href="/<?= $site->language() ?>/?section=<?= $item->slug(); ?>"><span><?= $item->title()->html() ?><span></a>
 					</li>
 				<?php endif; ?>
-			<?php endif; ?>
-		<?php endforeach; ?>
-	</ul>
-	<ul class="Navigation__third">
-		<?php foreach($site->page('home')->children()->visible() as $item): ?>
-			<?php if($item->navigation() == "third"): ?>
-				<li>
-					<a href="/<?= $site->language() ?>/?section=<?= $item->slug(); ?>"><span><?= $item->title()->html() ?><span></a>
-				</li>
-			<?php endif; ?>
-		<?php endforeach; ?>
-	</ul>
+			<?php endforeach; ?>
+		</ul>
+	</div>
 </nav>
