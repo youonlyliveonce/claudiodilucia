f268408aed82061c0a2858687ad06d50960c294f
danielkremin <design@kremin.de>
2017-12-21 00:39:40 +0100

    Add Google ID

--- public/site/snippets/head.php
+++ public/site/snippets/head.php
@@ -25,7 +25,7 @@
 		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-		ga('create', 'UA-96532469-X', 'auto');
+		ga('create', 'UA-96990711-1', 'auto');
 		ga('set', 'anonymizeIp', true);
 		ga('send', 'pageview');
 	 </script>
