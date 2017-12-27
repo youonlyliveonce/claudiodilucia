a81f6f0e84f778f7131d28161764752025f50554
danielkremin <design@kremin.de>
2017-12-27 22:22:18 +0100

    Update Instagram

--- public/site/snippets/section/instagram.php
+++ public/site/snippets/section/instagram.php
@@ -1,12 +1,12 @@
 <div class="Instagram [ Element ]" id="<?php echo $section->slug(); ?>_insta"  data-view="InstagramView">
 	<div class="Instagram__headline"><h2>Instagram</h2></div>
 	<div class="Instagram__body" id="instafeed">
-		
+
 	</div>
 	<div class="Next">
-		<a class="Button Button--down" href="http://instagram.com/claudiodilucia/">
+		<a class="Button Button--down" href="http://instagram.com/claudiodilucia.official/">
 			<span></span>
-			<span>instagram.com/claudiodilucia</span>
+			<span>instagram.com/claudiodilucia.official</span>
 		</a>
 	</div>
 </div>
